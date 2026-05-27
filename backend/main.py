7a50d8a1926223ae5a77b9388112b90bc72c6099
DanieLuque <Danielcaluque502@gmail.com>
2026-05-27 17:28:47 -0500

    feat: configuracion variables de entorno

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+JWT_SECRET = os.getenv("JWT_SECRET")
 
 app = FastAPI()
 
 @app.get("/")
-def inicio():
-    return {"mensaje": "Backend funcionando"}
+def root():
+    return {
+        "message": "Lichen Dreams API funcionando",
+        "db_host": DB_HOST
+    }
