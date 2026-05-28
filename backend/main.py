dcc48e7c1e601833751aacfbc31df62dffd4d93b
Heidy-Vivas <hvivas2208@gmail.com>
2026-05-27 22:51:03 -0500

    feat: configuracion inicial autenticacion jwt

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 from config.database import engine
+from auth.jwt_handler import create_token
+from passlib.context import CryptContext
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
@@ -24,4 +27,24 @@ def root():
     return {
         "message": "Lichen Dreams API funcionando",
         "db_host": DB_HOST
+    }
+
+@app.get("/token")
+def generate():
+    token = create_token({"user": "admin"})
+    return {"token": token}
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class PasswordRequest(BaseModel):
+    password: str = Field(..., min_length=1, max_length=72, description="Contraseña (máximo 72 caracteres)")
+
+@app.post("/registro")
+def registro(password: str):
+
+    hash_password = pwd_context.hash(password)
+
+    return {
+        "password_original": password,
+        "password_hash": hash_password
     }
